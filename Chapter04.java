73640c1ae8658d46a375fefda2a0ec1032970191
EC2 Default User <ayaka-31014313@outlook.jp>
2022-12-25 16:55:13 +0000

    Java演習基礎

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+	 public static void main(String[] args) {
+		String myname = "myname";
+		System.out.println(myname);
+
+		myname = "ポジティブ";
+		System.out.println(myname);
+
+    final int LACKY_NUMBER = 3;
+    System.out.println(LACKY_NUMBER);
+	 }
+}
