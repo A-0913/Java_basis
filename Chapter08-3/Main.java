3870dd67f77c6aef4d0fe22f13548119dde2f5bb
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-11 05:01:04 +0000

    基礎編8章確認問題3

--- Chapter08-3/Main.java
+++ Chapter08-3/Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    int i = 1;
-    while(i < 5){
-      int number = 0;
-      number = i*i;
-      System.out.println("number = " + number);
-      i++;
+    int[] array = { 10, 25, 40, 59 };
+    for(int number : array){
+      if(number % 2 == 0){
+        continue;
+      }
+      System.out.println(number);
     }
   }
 }
