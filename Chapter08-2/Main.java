9be5d25a5a4a7938daf2a5fd9e0f16d7d31bc5a9
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-11 04:49:55 +0000

    基礎編8章確認問題2

---
+++ Chapter08-2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] array = { 10, 20, 40, 50 };
+    for(int number = 0 ; number <= array.length; number++){
+      System.out.println(array[number]);
+    }
+  }
+}
