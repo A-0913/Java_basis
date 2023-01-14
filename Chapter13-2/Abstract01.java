d549bdb17ae88ba4d97caefdfaf785ffdc330017
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-14 04:57:04 +0000

    基礎編13章復習

---
+++ Chapter13-2/Abstract01.java
@@ -0,0 +1,22 @@
+package extend;
+
+import extend.animal.Dog;
+
+public class Abstract01 {
+    public static void main(String[] args) {
+
+        // Dogクラス(=サブクラス)のインスタンスを生成する
+        Dog dog = new Dog();
+
+        // 抽象クラス（Animalクラス）のメソッドを呼び出
+        // 抽象クラスのメソッドやフィールドにアクセスする場合は、
+        // 必ずサブクラスのインスタンスを生成してからアクセスする
+        dog.eat();
+
+        // 抽象メソッドを実装したメソッドを呼び出す（※Dogクラスでオーバーライト済み）
+        dog.cry();
+
+        // 抽象クラスはインスタンス生成することができないため、下記はコンパイルエラーになる
+        // Animal animal = new Animal();
+    }
+}
---
+++ Chapter13-2/Animal.java
@@ -0,0 +1,15 @@
+package extend.animal;
+
+//abstract修飾子を設定して、抽象クラスとして宣言する
+public abstract class Animal {
+    protected String name;
+
+    // public→どこからでもアクセス可能
+    public void eat() {
+        System.out.println(name + "が食事をしました！");
+    }
+
+    // サブクラスごとに違う処理は、抽象メソッドにしてサブクラスに実装させる
+    // 継承したサブクラスにてオーバーライドしないと継承したサブクラスになる
+    public abstract void cry();
+}
