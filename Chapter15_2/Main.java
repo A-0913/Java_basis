b05271fb8831f447a088f0ceb57eec41d86c3bb4
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-22 08:25:54 +0000

    基礎編15章確認問題2

--- Chapter15_2/Main.java
+++ Chapter15_2/Main.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 // BufferedWriter:文字をバッファリングすることによって、文字、配列、または文字列を効率良く文字型出力ストリームに書き込む
 import java.io.BufferedWriter;
 // IOException:入出力処理中の例外を管理するクラス
+// 何らかの理由で「書き込みのために」開くことが出来なかった場合には、IOExceptionが発生する
 import java.io.IOException;
 
 // java.nioパッケージ:基本的な標準I/O処理を補完した形で、より高機能な処理を実現出来るよう改善されたパッケージ
@@ -21,38 +22,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-    public static void main(String[] args) {
-        FileSystem fs = FileSystems.getDefault();
-        // PathクラスのインスタンスをFileSystem.getPathメソッドで生成
-        Path inputPath = fs.getPath("resouces/input.txt");
-        Path outputPath = fs.getPath("resouces/output.txt");
+  public static void main(String[] args) {
+    FileSystem fs = FileSystems.getDefault();
+    // PathクラスのインスタンスをFileSystem.getPathメソッドで生成
+    Path inputPath = fs.getPath("resouces/input.txt");
+    Path outputPath = fs.getPath("resouces/output.txt");
 
-        // Java7以降に追加された「try-with-resources」文で処理
-        // Filesクラスを使用して、BufferedReaderクラスのインスタンスを生成
+    // Java7以降に追加された「try-with-resources」文で処理
+    // Filesクラスを使用して、BufferedReaderクラスのインスタンスを生成
+    try (BufferedReader reader = Files.newBufferedReader(inputPath);
         // Filesクラスを使用して、BufferedWriterクラスのインスタンスを生成
-        try (BufferedReader reader = Files.newBufferedReader(inputPath);
-                BufferedWriter writer = Files.newBufferedWriter(outputPath);) {
+        BufferedWriter writer = Files.newBufferedWriter(outputPath);) {
 
-            String line = null;
-            // BufferedReader.readLineメソッドを使用して、1行づつ処理
-            // 読み込む行がなくなるまでループ
-            while ((line = reader.readLine()) != null) {
-                // 文字数が5以外の場合はスキップ
-                if (line.length() != 5) {
-                    continue;
-                }
-                // 読み込んだ1行を出力
-                writer.write(line);
-                // 改行がついてないので改行を出力
-                writer.newLine();
-            }
+      String line = null;
+      // BufferedReader.readLineメソッドを使用して、1行づつ処理
+      // 読み込む行がなくなるまでループ
+      while ((line = reader.readLine()) != null) {
+          // 文字数が5以外の場合はスキップ
+          if (line.length() != 5) {
+              continue;
+          }
+          // 読み込んだ1行を出力
+          writer.write(line);
+          // 改行がついてないので改行を出力
+          writer.newLine();
+      }
 
-            // 例外処理を記述
-            // printStackTraceで例外を出力
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("出力処理終了");
-        }
+    // 例外処理を記述
+    // printStackTraceで例外を出力
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      System.out.println("出力処理終了");
     }
+  }
 }
