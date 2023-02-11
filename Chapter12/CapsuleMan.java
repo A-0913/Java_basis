71192dfa3296030f7fc07a92bed466f1f254b5d8
EC2 Default User <ayaka-31014313@outlook.jp>
2023-02-11 08:09:42 +0000

    基礎編12章演習問題復習

--- Chapter12/CapsuleMan.java
+++ Chapter12/CapsuleMan.java
@@ -12,18 +12,37 @@ public class CapsuleMan {
     this.age = age;
   }
 
-  // nameのgetterメソッド:private になっているフィールド変数を取得するためのもの
+  // nameのgetterメソッド:private になっているフィールド変数nameを取得するためのもの
   public String getName() {
     return this.name;
   }
 
-  // nameのsetterメソッド:private になっているフィールド変数を変更するためのもの
+  // nameのsetterメソッド:private になっているフィールド変数ageを変更するためのもの
   public int getAge() {
     return this.age;
   }
 
-  // ageのgetterメソッド:private になっているフィールド変数を取得するためのもの
+  // ageのgetterメソッド:private になっているフィールド変数ageを取得するためのもの
   public void setAge(int age) {
     this.age = age;
   }
 }
+
+
+/* 【getter/setter】
+  フィールドにアクセス制限を「private」にすることで、
+  他のプログラムによる直接の変更を禁止できるようになるが、  同時に値の取得もできなくなってしまう。
+  そのため、フィールドの値を取得するための「getter」メソッドや
+  値を変更するための「setter」メソッドを必要に応じて作成して、他のプログラムから参照できるようにする。
+
+  【基本構文】
+    getterメソッド
+      public 戻り値の型(フィールドの型)  getフィールド名() {
+        return this.フィールド名
+      }
+
+    setterメソッド
+      public void setフィールド名(フィールドの型の引数) {
+        this.フィールド名 = フィールドの型の引数;
+      }
+*/
