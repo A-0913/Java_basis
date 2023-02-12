848082240eff3b02a35f0d70e6d5851646e446cb
EC2 Default User <ayaka-31014313@outlook.jp>
2023-02-12 01:13:37 +0000

    基礎編13章復習

--- Chapter13-2/Abstract01.java
+++ Chapter13-2/Abstract01.java
@@ -1,22 +1,20 @@
-package extend;
-
 import extend.animal.Dog;
 
 public class Abstract01 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        // Dogクラス(=サブクラス)のインスタンスを生成する
-        Dog dog = new Dog();
+    // Dogクラス(=サブクラス)のインスタンスを生成する
+    Dog dog = new Dog();
 
-        // 抽象クラス（Animalクラス）のメソッドを呼び出す
-        // 抽象クラスのメソッドやフィールドにアクセスする場合は、
-        // 必ずサブクラスのインスタンスを生成してからアクセスする
-        dog.eat();
+    // 抽象クラス（Animalクラス）のメソッドを呼び出す
+    // 抽象クラスのメソッドやフィールドにアクセスする場合は、
+    // 必ずサブクラスのインスタンスを生成してからアクセスする
+    dog.eat();
 
-        // 抽象メソッドを実装したメソッドを呼び出す（※Dogクラスでオーバーライト済み）
-        dog.cry();
+    // 抽象メソッドを実装したメソッドを呼び出す（※Dogクラスでオーバーライト済み）
+    dog.cry();
 
-        // 抽象クラスはインスタンス生成することができないため、下記はコンパイルエラーになる
-        // Animal animal = new Animal();
-    }
+    // 抽象クラスはインスタンス生成することができないため、下記はコンパイルエラーになる
+    // Animal animal = new Animal();
+  }
 }
--- Chapter13-2/Animal.java
+++ Chapter13-2/Animal.java
@@ -1,15 +1,13 @@
-package extend.animal;
-
 //abstract修飾子を設定して、抽象クラスとして宣言する
 public abstract class Animal {
-    protected String name;
+  protected String name;
 
-    // public→どこからでもアクセス可能
-    public void eat() {
-        System.out.println(name + "が食事をしました！");
-    }
+  // public→どこからでもアクセス可能
+  public void eat() {
+    System.out.println(name + "が食事をしました！");
+  }
 
-    // サブクラスごとに違う処理は、抽象メソッドにしてサブクラスに実装させる
-    // 継承したサブクラスにてオーバーライドしないとコンパイルエラーになる
-    public abstract void cry();
+  // サブクラスごとに違う処理は、抽象メソッドにしてサブクラスに実装させる
+  // 継承したサブクラスにてオーバーライドしないとコンパイルエラーになる
+  public abstract void cry();
 }
