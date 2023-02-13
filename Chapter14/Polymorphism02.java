8881fe9529a1fde865dafda99692238c6d2cdc91
EC2 Default User <ayaka-31014313@outlook.jp>
2023-02-13 03:25:38 +0000

    基礎編14章復習

--- Chapter14/Polymorphism02.java
+++ Chapter14/Polymorphism02.java
@@ -1,4 +1,5 @@
 public class Polymorphism02 {
+
   public static void main(String[] args) {
     // スーパークラス（Humanクラス）の変数にインスタンスを代入
     Human human = new Student("佐藤", 17, 70);
@@ -19,16 +20,27 @@ public class Polymorphism02 {
   public void printName() {
     System.out.println("中川です。");
   }
+  // 出力結果:中川です。
 
   public void printName(String name) {
     System.out.println(name + "です。");
   }
+  // 出力結果:松本です。
 
   public void printName(String name1, String name2) {
     System.out.println(name1 + "と" + name2 + "です。");
   }
+  // 出力結果:松本と清水です。
 
   public void printName(Human human) {
     System.out.println(human.getName() + "です。");
   }
+  // 出力結果:佐藤です。
 }
+
+/*オーバーロードとは？
+
+  引数の型、数、並び順のいずれかが異なる同じ名前のメソッドを１つのクラスに複数定義すること。
+  同じ処理のメソッドを同じメソッド名で定義できるため、ソースコードがわかりやすくできる。
+
+*/
